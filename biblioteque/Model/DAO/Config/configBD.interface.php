d6263a3a4666095c62e675668b473f4b7681bc7e
Handy Charles G. Pierre <hpierre@dept-info.crosemont.quebec>
2023-11-11 10:09:49 -0500

    Mis en place des configurations pour la connexion a la base de donnée

---
+++ biblioteque/Model/DAO/Config/configBD.interface.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Interface pour la configuration de la base de donné
+ * pramètres (hostname,userName,password,dataBaseName)
+ */
+interface ConfigurationBD
+{
+    const BD_HOST = "localhost";
+    const BD_USER = "root";
+    const BD_PASS_WORD= "@Luisetmoi103";
+    const BD_NAME = "bibliotheque_departemental";
+}
