4de96aebac300607bbc8dd59525e33a3f4d3bc48
Lleyton Habimana <@lhabimana>
2023-11-13 19:04:38 -0500

    ajout des classes departement.php et notification.php

---
+++ biblioteque/Model/Departement.php
@@ -0,0 +1,71 @@
+<?php
+class Departement {
+    private $utilisateurs = [];
+    private $administrateur;
+    private $livres = [];
+    private $listes = [];
+    private $demande = [];
+    private $notifications = [];
+
+    public function __construct($utilisateurs, $administrateur, $livres, $listes, $demande, $notifications) {
+        $this->utilisateurs = $utilisateurs;
+        $this->administrateur = $administrateur;
+        $this->livres = $livres;
+        $this->listes = $listes;
+        $this->demande = $demande;
+        $this->notifications = $notifications;
+    }
+
+    public function getUtilisateurs() {
+        return $this->utilisateurs;
+    }
+
+    public function setUtilisateur($utilisateur) {
+        $this->utilisateurs[] = $utilisateur;
+    }
+
+    public function getAdministrateur() {
+        return $this->administrateur;
+    }
+
+    public function envoyerNotification($notification, $destinataire) {
+        // ...
+    }
+
+    public function getLivres() {
+        return $this->livres;
+    }
+
+    public function ajouterUnLivre($unLivre) {
+        $this->livres[] = $unLivre;
+    }
+
+   
+
+    public function setLivres($livres) {
+        $this->livres = $livres;
+    }
+
+   
+
+    public function rechercherUnLivre($criteres) {
+        //...
+    }
+
+    public function getDemande() {
+        return $this->demande;
+    }
+
+    public function setDemande($demande) {
+        $this->demande = $demande;
+    }
+
+    public function getNotifications() {
+        return $this->notifications;
+    }
+
+    public function setNotifications($notifications) {
+        $this->notifications = $notifications;
+    }
+}
+?>
